b012f8786c04991c7609dc37fde4016013419377
eiwashita <erikoiwashita@gmail.com>
2026-07-09 02:49:15 -0600

    Create a CRUD application in React

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,78 @@
+import { CssBaseline, Container, Box } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  };
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  };
+
+  const handleOpenForm = (id?: string) => {
+    if (id) {
+      handleSelectActivity(id);
+    } else {
+      handleCancelSelectActivity();
+    }
+    setEditMode(true);
+  };
+
+  const handleCloseForm = () => {
+    setEditMode(false);
+  };
+
+  const handleSubmitForm = (activity: Activity) => {
+    if (activity.id) {
+      setSelectedActivity(activity);
+      setActivities(activities.map(x => (x.id === activity.id ? activity : x)));
+    } else {
+      const newActivity = { ...activity, id: activities.length.toString() };
+      setSelectedActivity(newActivity);
+      setActivities([...activities, newActivity]);
+    }
+    setEditMode(false);
+  };
+
+  const handleDelete = (id: string) => {
+    setActivities(activities.filter(x => x.id !== id));
+  };
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data));
+  }, []);
+
+  return (
+    <Box sx={{ bgcolor: '#eee' }}>
+      <CssBaseline />
+      <NavBar openForm={handleOpenForm} />
+      <Container maxWidth="xl" sx={{ mt: 3 }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          openForm={handleOpenForm}
+          closeForm={handleCloseForm}
+          submitForm={handleSubmitForm}
+          deleteActivity={handleDelete}
+        />
+      </Container>
+    </Box>
+  );
+}
+
+export default App;
