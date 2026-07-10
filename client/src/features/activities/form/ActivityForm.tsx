ae48f9b049656ff6b53d0c21515806c616691b76
eiwashita <erikoiwashita@gmail.com>
2026-07-09 20:32:13 -0600

    Add React Query and Axios

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,16 @@
 import type { SyntheticEvent } from 'react';
 import { Box, Button, Paper, TextField, Typography } from '@mui/material';
+import { useActivities } from '../../../lib/hooks/useActivities';
 
 type Props = {
   activity?: Activity;
   closeForm: () => void;
-  submitForm: (activity: Activity) => void;
 };
 
-const ActivityForm = ({ activity, closeForm, submitForm }: Props) => {
-  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+const ActivityForm = ({ activity, closeForm }: Props) => {
+  const { updateActivity, createActivity } = useActivities();
+
+  const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
@@ -18,9 +20,14 @@ const ActivityForm = ({ activity, closeForm, submitForm }: Props) => {
       data[key] = value;
     });
 
-    if (activity) data.id = activity.id;
-
-    submitForm(data as unknown as Activity);
+    if (activity) {
+      data.id = activity.id;
+      await updateActivity.mutate(data as unknown as Activity);
+      closeForm();
+    } else {
+      await createActivity.mutate(data as unknown as Activity);
+      closeForm();
+    }
   };
 
   return (
@@ -49,7 +56,11 @@ const ActivityForm = ({ activity, closeForm, submitForm }: Props) => {
         <TextField
           name="date"
           label="Date"
-          defaultValue={activity?.date}
+          defaultValue={
+            activity?.date
+              ? new Date(activity.date).toISOString().split('T')[0]
+              : new Date().toISOString().split('T')[0]
+          }
           type="date"
         />
         <TextField name="city" label="City" defaultValue={activity?.city} />
@@ -58,7 +69,12 @@ const ActivityForm = ({ activity, closeForm, submitForm }: Props) => {
           <Button onClick={closeForm} color="inherit">
             Cancel
           </Button>
-          <Button type="submit" color="success" variant="contained">
+          <Button
+            type="submit"
+            color="success"
+            variant="contained"
+            disabled={updateActivity.isPending || createActivity.isPending}
+          >
             Submit
           </Button>
         </Box>
